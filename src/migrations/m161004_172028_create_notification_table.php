26bac8bb036e3e776ee337edc188108cf24eed01
Aleksandar Panic <arekusanda1@gmail.com>
2018-08-30 12:26:22 +0200

    fix migration

--- src/migrations/m161004_172028_create_notification_table.php
+++ src/migrations/m161004_172028_create_notification_table.php
@@ -17,14 +17,12 @@ class m161004_172028_create_notification_table extends Migration
             'user_id' => $this->integer()
         ]);
 
-        $this->createIndex('notification_created_by_indx', 'notification', 'created_by');
-        $this->createIndex('notification_updated_by_indx', 'notification', 'updated_by');
+        $this->createIndex('notification_user_id_indx', 'notification', 'user_id');
     }
 
     public function down()
     {
-        $this->dropIndex('notification_created_by_indx', 'notification');
-        $this->dropIndex('notification_updated_by_indx', 'notification');
+        $this->dropIndex('notification_user_id_indx', 'notification');
         $this->dropTable('notification');
     }
 }
