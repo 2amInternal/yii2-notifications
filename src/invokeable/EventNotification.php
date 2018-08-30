ccc1b5b356ece6da0e756e8d3a49c62495057914
Aleksandar Panic <arekusanda1@gmail.com>
2018-08-30 10:25:10 +0200

    Added info for event notifications.

--- src/invokeable/EventNotification.php
+++ src/invokeable/EventNotification.php
@@ -9,16 +9,51 @@ namespace dvamigos\Yii2\Notifications\invokeable;
 
 use dvamigos\Yii2\Notifications\NotificationComponent;
 use Yii;
+use yii\base\Event;
 
 abstract class EventNotification extends \yii\base\BaseObject
 {
-    /** @var string|callable */
+    /**
+     *  Type of the notification
+     *
+     *  If string is passed then that notification type will be used.
+     *
+     *  If callable function then that function will be called to retrieve the value which must be string or false.
+     *
+     *  Callable function should be in format:
+     *  function($eventNotification) {
+     *      return 'type';
+     *  }
+     *
+     *  If value passed is false then this notification will not be resolved (i.e. resolve() will not be called).
+     *
+     *  @var string|callable|bool
+     */
     public $type = '';
 
-    /** @var array|callable */
+    /**
+     *  Data for notification type which will be saved.
+     *
+     *  If array, then that associative array will be used.
+     *  If callable then that function will be called to get the array value.
+     *
+     *  Callable function should be in format:
+     *  function($eventNotification) {
+     *      return ['mydata' => true];
+     *  }
+     *
+     *  @var array|callable
+     */
     public $data = [];
 
-    /** @var string */
+    /**
+     * Notification manager component which will be used in getManager()
+     *
+     * If string then this will be resolved to manager component using Yii::$app->get()
+     * If NotificationComponent then that component will be used directly.
+     *
+     * @var string|NotificationComponent
+     */
     public $notificationManager = 'notification';
 
     public function __invoke(\yii\base\Event $event)
@@ -26,19 +61,32 @@ abstract class EventNotification extends \yii\base\BaseObject
         $type = is_callable($this->type) ? call_user_func($this->type, $this) : $this->type;
         $data = is_callable($this->data) ? call_user_func($this->data, $this) : $this->data;
 
+        if (!is_string($type)) {
+            return;
+        }
+
         $this->resolve($event, $type, $data);
     }
 
     /**
+     * Returns current notification manager assigned to this event notification.
+     *
      * @return NotificationComponent
      * @throws \yii\base\InvalidConfigException
      */
     public function getManager()
     {
         /** @var NotificationComponent $manager */
-        $manager = Yii::$app->get($this->notificationManager);
+        $manager = is_string($this->notificationManager) ? Yii::$app->get($this->notificationManager) : $this->notificationManager;
         return $manager;
     }
 
-    public abstract function resolve(\yii\base\Event $event, $type, $data);
+    /**
+     * Resolves the notification when this notification its called.
+     *
+     * @param Event $event Event object on which this notification is called
+     * @param $type string Type of the notification.
+     * @param $data array Data of the notification
+     */
+    public abstract function resolve(Event $event, $type, $data);
 }
--- src/invokeable/ReplaceNotification.php
+++ src/invokeable/ReplaceNotification.php
@@ -9,15 +9,29 @@ namespace dvamigos\Yii2\Notifications\invokeable;
 
 use dvamigos\Yii2\Notifications\NotificationComponent;
 use Yii;
+use yii\base\Event;
 use yii\di\Instance;
 
 class ReplaceNotification extends EventNotification
 {
-    /** @var callable|int */
+    /**
+     * ID of the notification which will be replaced.
+     *
+     * If value is numeric then that value will be used.
+     *
+     * If callable then that function will be called to get the ID. ID must be a numeric value.
+     *
+     * Callable function should be in format:
+     * function($eventNotification) {
+     *     return 1; // Notification ID
+     * }
+     *
+     * @var callable|int
+     */
     public $replaceId;
 
     /**
-     * Pushes notification to using notification manager.
+     * Replaces existing notification using notification manager.
      *
      * @param \yii\base\Event $event
      * @param $type
@@ -26,7 +40,7 @@ class ReplaceNotification extends EventNotification
      * @throws \yii\base\Exception
      * @throws \yii\base\InvalidConfigException
      */
-    public function resolve(\yii\base\Event $event, $type, $data)
+    public function resolve(Event $event, $type, $data)
     {
         $id = is_callable($this->replaceId) ? call_user_func($this->replaceId, $this) : $this->replaceId;
         $this->getManager()->replace($id, $type, $data);
